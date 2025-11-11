d49fd756c0b5cc209df62734c531973cfa5f3426
kausiq <kausiqmondol@gmail.com>
2025-11-11 14:17:18 +0600

    2nd commit

---
+++ db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb')
+
+const uri = process.env.MONGODB_URI
+if (!uri) throw new Error('Missing MONGODB_URI in .env')
+
+const client = new MongoClient(uri, { ignoreUndefined: true })
+let _db
+
+async function getDb() {
+  if (!_db) {
+    await client.connect()
+    _db = client.db(process.env.DB_NAME || 'ai_inventory')
+  }
+  return _db
+}
+
+module.exports = { getDb, client }
